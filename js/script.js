f63a28cb3a31a44f588d3cd8485973f3d6b9b9bd
Bridget Hapner <bridgethapner@gmail.com>
2022-04-05 19:03:24 -0600

    adding js to file from my codepen test sandbox

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 function init(){
 //add your javascrip between these two lines of code
+const btn = document.getElementById('entrybutton');
+function modifyText() {
+  const results = document.getElementsByTagName('h2')[1];
+   if (document.getElementById('entryinput').value == null || document.getElementById('entryinput').value == '') {
+    results.firstChild.nodeValue = "Show results here";
+  } else {
+    results.firstChild.nodeValue = document.getElementById('entryinput').value;
+  }
+}
+
+btn.addEventListener('click', () => {
+  window.alert(
+    document.getElementsByTagName('h2')[0].innerText + ': ' +
+    document.getElementById('entryinput').value);
+});
+btn.addEventListener('click', modifyText, false);
+
+  
+
+
+
+
  
 
 
